78cd786a3324f056f2975e964ff9e4cd002e3df0
AK4PLC <122453697+AK4PLC@users.noreply.github.com>
2023-01-17 16:57:55 +0300

    Add files via upload

---
+++ sc1.js
@@ -0,0 +1,40 @@
+"use strict";
+
+//let isYap = confirm("?", '');
+//alert(isYap);
+
+//let isHip = prompt('ru?', 100);
+
+//if (isHip > 11) {
+//    alert(true)
+//    }
+//else {alert("false");
+//}
+//alert(isHip);
+//let a = 1, b = 0, c;
+//((c = a + b) < 2) ?
+//    alert("true "+c) : alert("false "+c);
+//document.write("HW");
+//document.createElement('pict');
+//$(".drag").draggable();
+//$(".drag").droppable();
+//var takeScreenShot = function () {
+//    html2canvas(document.getElementById("container"), {
+//        onrendered: function (canvas) {
+//            var tempcanvas = document.createElement('canvas');
+//            tempcanvas.width = 350;
+//            tempcanvas.height = 350;
+//            var context = tempcanvas.getContext('2d');
+//            context.drawImage(canvas, 112, 0, 288, 200, 0, 0, 350, 350);
+//            var link = document.createElement("a");
+//            link.href = tempcanvas.toDataURL('image/jpg');   //function blocks CORS
+//            link.download = 'screenshot.jpg';
+//            link.click();
+//        }
+//    });
+//}
+//alert(typeof null);
+//alert(`${1+1}`);
+    /*
+    <hr />
+    */
